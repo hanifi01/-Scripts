a625b2d0f7b4d0b8809ea44087384ae2f6102163
Abdullah <harrisgold01@gmail.com>
2024-10-15 23:52:56 +0000

    update push4.py

---
+++ Ahmad/push4.py
@@ -0,0 +1,15 @@
+x= 22
+print(x)
+
+type(x)
+int
+y= "Minit chocolate chip"
+print(y)
+
+print(x)
+
+type(y)
+str
+y="chocolate"
+Y="Mint chocolate chip"
+print(Y)
---
+++ python/operations.py
@@ -0,0 +1,32 @@
+import inquirer
+
+question = [
+    inquirer.List('operator',
+                  message="Please choose an operator:",
+                  choices=["+", "*", "/", "//", "**","-"],
+                  default="+"),
+    inquirer.Text("num1", message="Enter first number:"),
+    inquirer.Text("num2", message="Enter second number:"),
+]
+
+
+
+answers = inquirer.prompt(question)
+if answers['operator'] == '+':
+    result = int(answers['num1']) + int(answers['num2'])
+    print("Answer:", result)
+
+
+if answers['operator'] == '*':
+    result = int(answers['num1']) * int(answers['num2'])
+    print("Answer:", result)
+
+if answers['operator'] == '/':
+    result = int(answers['num1']) / int(answers['num2'])
+    print("Answer:", result)
+
+
+if answers['operator'] == '-':
+    result = int(answers['num1']) - int(answers['num2'])
+    print("Answer:", result)
+
