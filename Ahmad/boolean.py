d80c63d8b044d070fa1f6b546e1db2e629487f2b
Abdullah <harrisgold01@gmail.com>
2024-10-16 03:26:47 +0000

    update boolean.py

---
+++ Ahmad/boolean.py
@@ -0,0 +1,16 @@
+
+is_greater= 5> 3
+
+print(is_greater)
+
+is_less = 2< 1
+
+print(is_less)
+
+is_raining = False
+
+if is_raining:
+    print("Take an umbrella!")
+else:
+    print("No umbrella needed")
+
