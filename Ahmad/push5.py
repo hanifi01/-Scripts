5d46d520205cb60782598b7d46a9627712f0babd
Abdullah <harrisgold01@gmail.com>
2024-10-16 00:50:37 +0000

    update push5.py

---
+++ Ahmad/push5.py
@@ -0,0 +1,19 @@
+
+x, y, z = 'ice', 'Vanilla', 'High Road'
+
+print(x)
+print(y)
+print(z)
+
+x= y = z = 'Rocky Coke ice'
+
+print(x)
+print(y)
+print(z)
+
+ice_cream = ['ice','Vanilla','High way',]
+x,y,z = ice_cream
+
+print(x)
+print(y)
+print(z)
