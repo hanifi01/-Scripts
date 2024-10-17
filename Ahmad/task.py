cfb61572099590fd927582101eb88164374ecf01
Abdullah <harrisgold01@gmail.com>
2024-10-17 20:41:01 +0000

    update task.py

---
+++ Ahmad/task.py
@@ -0,0 +1,33 @@
+
+print("Abdullah")
+print("3")
+
+
+greeting = "Hello " + "world"
+print(greeting)
+
+
+age_input = input("Please enter your age: ")
+
+
+age_int = int(age_input)
+age_float = float(age_input)
+
+
+print("Age as integer:", age_int)
+print("Age as float:", age_float)
+print("Age as string:", age_input)
+
+
+first_name = "Abdullah"
+last_name = "Hanifi"
+age = 36
+
+
+print(first_name, last_name, age, sep=", ")
+
+
+for i in range(1, 6):
+    print(i, end=' ')
+
+
