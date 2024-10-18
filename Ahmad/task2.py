3eb5f7b7464092c4bbca8da26dfd94253a6554f4
Abdullah <harrisgold01@gmail.com>
2024-10-18 22:09:22 +0000

    update task2.py

---
+++ Ahmad/task2.py
@@ -0,0 +1,12 @@
+
+name = input("What is your name?")
+print(" Hello " + name)
+
+birth_year = input("Enter your birth year:")
+age = 2024 - int(birth_year)
+print (age)
+
+first = input("First:")
+second = input("Second:")
+sum = float(first)+ float (second)
+print(sum)
