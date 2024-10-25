a4b996c9a5a934b8b3a2a0769407d672655c7139
Abdullah <harrisgold01@gmail.com>
2024-10-25 23:10:02 +0000

    update p.py

---
+++ Ahmad/p.py
@@ -0,0 +1,69 @@
+
+print ("Abdullah")
+age = 36
+height = 6.2
+color ="brown"
+size = 43
+#print ("Age:", age, "Height:", height, "Color:", color, "Size:", size)
+
+print("Age:", age)
+print("Height:", height)
+print("color", color)
+print("Size", size)
+
+print ("Hello", "World")
+
+result = 5 + 4 
+print(result)
+
+
+new_age = age + 1 
+print("Next year, age will be:", new_age)
+
+x = 15
+y = 4 
+print(x // y)
+
+age - 18
+if age >= 18:
+    print("You are an adult")
+else:
+        print("You are not an adult")
+
+
+#time = "7:30"  
+
+#if time == "7:30":  
+    #print("It's party time.")
+#else:  
+    #print("It's not party time.")
+
+
+time = input("Enter the time (in HH:MM AM/PM format): ")
+
+if (time >= "9:00 PM" and time <= "11:59 PM") or (time >= "12:00 AM" and time <= "3:00 AM"):
+    print("It's party time.")
+else:  
+    print("It's not party time.")
+
+
+    number = 5
+
+if number >= 0:  
+    print("This number is positive.")
+else:
+    print("This number is negative.")
+
+
+
+name = ""
+
+if name == "":
+    print("The name is Abdullah.")
+
+
+    num = 10
+    if num % 2 == 0:
+        print("the number is even")
+
+        
