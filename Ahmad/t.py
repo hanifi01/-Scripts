c0b24d084ae38c31cebea20fac61a576f07e7645
Abdullah <harrisgold01@gmail.com>
2024-10-28 23:53:06 +0000

    update t.py

---
+++ Ahmad/t.py
@@ -0,0 +1,34 @@
+#num1 = int(input("Enter the first number:"))
+#num2 = int(input("Enter the second number:"))
+#sum = num1 + num2 
+#print("The sum is:",sum)
+
+
+
+
+# age_years = int(input("Enter your age in years: "))
+# age_months = age_years * 12
+# print("Your age in months is:", age_months)
+
+
+
+# age = int(input("Enter your age in number:"))
+# if age>= 18:
+#     print("you can vote!")
+# else:
+#     print("you can not vote yet.")
+
+# celsius = int(input("Enter temperatur in Celsius:"))
+# fahrenheit = (celsius * 9/5) + 32 
+# print("Temperature i Fahrenheit is:", fahrenheit) 
+
+score = int(input("Enter your score:"))
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")    
+elif score >= 70:
+    print("Grade: C")    
+else: 
+    print("Grade: D.")    
