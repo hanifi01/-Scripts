4b1e1b8677eab393b1bfa292e5fad8619cb346b3
Abdullah <harrisgold01@gmail.com>
2024-10-30 23:35:41 +0000

    update t.py

--- Ahmad/t.py
+++ Ahmad/t.py
@@ -1,26 +1,26 @@
-#num1 = int(input("Enter the first number:"))
-#num2 = int(input("Enter the second number:"))
-#sum = num1 + num2 
-#print("The sum is:",sum)
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+sum = num1 + num2 
+print("The sum is:",sum)
 
 
 
 
-# age_years = int(input("Enter your age in years: "))
-# age_months = age_years * 12
-# print("Your age in months is:", age_months)
+age_years = int(input("Enter your age in years: "))
+age_months = age_years * 12
+print("Your age in months is:", age_months)
 
 
 
-# age = int(input("Enter your age in number:"))
-# if age>= 18:
-#     print("you can vote!")
-# else:
-#     print("you can not vote yet.")
+age = int(input("Enter your age in number:"))
+if age>= 18:
+    print("you can vote!")
+else:
+    print("you can not vote yet.")
 
-# celsius = int(input("Enter temperatur in Celsius:"))
-# fahrenheit = (celsius * 9/5) + 32 
-# print("Temperature i Fahrenheit is:", fahrenheit) 
+celsius = int(input("Enter temperatur in Celsius:"))
+fahrenheit = (celsius * 9/5) + 32 
+print("Temperature i Fahrenheit is:", fahrenheit) 
 
 score = int(input("Enter your score:"))
 
@@ -32,3 +32,17 @@ elif score >= 70:
     print("Grade: C")    
 else: 
     print("Grade: D.")    
+
+
+def fun(a):
+    global x
+    x += a    
+ 
+x = 2
+fun(3)
+
+fruits = ["apple", "banana","cherry"]
+for fruit in fruits:
+    print(fruit)
+
+
