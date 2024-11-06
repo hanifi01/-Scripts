55e7ff9373904b205ac3008446684c509be7d88b
Abdullah <harrisgold01@gmail.com>
2024-11-06 00:40:07 +0000

    update r.py

---
+++ Ahmad/r.py
@@ -0,0 +1,55 @@
+
+# age = 18
+
+# if age >= 18:
+# print("You can vote!")
+
+age = 18
+
+if age >= 18:
+    print("You can vote!")
+
+age = 16
+if age >= 18:
+    print("You are an adult!")
+else: 
+    print("You are a minor!")    
+
+age = 25 
+if age >= 18:
+    print("You are an adult!")
+elif age < 18 and age >= 13:
+    print("You are a teenager!")    
+else: 
+    print("You are a child!")   
+
+age = 20 
+has_ticket = True 
+
+if age >= 18:
+    if has_ticket:
+       print("You can enter the movie!") 
+    else:
+        print("You need a ticket to enter.")   
+else:
+    print("You are too young to enter.")        
+
+
+x = 10 
+y = 20
+
+if x == y: 
+    print("x is equal to y")
+elif x < y:
+     print("x is less then y")
+else: 
+     print("x is greater then y")   
+
+     x = 10
+     y = 20 
+if x == y:
+          print("x is equal to y")
+elif x < y:
+         print("x is less then y")      
+else: 
+         print("x is greater then y ")    
--- python/operations.py
+++ python/operations.py
@@ -1,32 +1,3 @@
-import inquirer
 
-question = [
-    inquirer.List('operator',
-                  message="Please choose an operator:",
-                  choices=["+", "*", "/", "//", "**","-"],
-                  default="+"),
-    inquirer.Text("num1", message="Enter first number:"),
-    inquirer.Text("num2", message="Enter second number:"),
-]
-
-
-
-answers = inquirer.prompt(question)
-if answers['operator'] == '+':
-    result = int(answers['num1']) + int(answers['num2'])
-    print("Answer:", result)
-
-
-if answers['operator'] == '*':
-    result = int(answers['num1']) * int(answers['num2'])
-    print("Answer:", result)
-
-if answers['operator'] == '/':
-    result = int(answers['num1']) / int(answers['num2'])
-    print("Answer:", result)
-
-
-if answers['operator'] == '-':
-    result = int(answers['num1']) - int(answers['num2'])
-    print("Answer:", result)
-
+if 7 <= 10:
+    print("yes")
